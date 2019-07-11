a7ea895de362eaecb869d10bde7f77c9f926928c
fahrisalfr <fahrisalfr@gmail.com>
2019-07-11 18:33:00 +0700

    Update Script Models Buku

--- Models/Buku.js
+++ Models/Buku.js
@@ -5,10 +5,6 @@ const sequelize = require('../Configs/Sequelize');
 class Buku extends Sequelize.Model {}
 
 Buku.init({
-  Id: { Sequelize.INTEGER,
-		autoIncrement: true,
-		primaryKey: true
-	},
   Nama: Sequelize.STRING,
   Halaman: Sequelize.INTEGER,
   Stok: Sequelize.INTEGER,
